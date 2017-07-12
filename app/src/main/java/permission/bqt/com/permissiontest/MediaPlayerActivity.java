fedbf7db06141a33c692577b7bee61067fe5c30f
baiqiantao <baiqiantao@sina.com>
2017-07-12 21:08:50 +0800

    2017年7月12日21:08:42

--- app/src/main/java/permission/bqt/com/permissiontest/MediaPlayerActivity.java
+++ app/src/main/java/permission/bqt/com/permissiontest/MediaPlayerActivity.java
@@ -1,12 +1,10 @@
 package permission.bqt.com.permissiontest;
 
-import android.annotation.TargetApi;
 import android.app.ListActivity;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.text.Spannable;
@@ -30,19 +28,16 @@ public class MediaPlayerActivity extends ListActivity {
 	private MediaPlayer mediaPlayer;
 	private static final int STATE_CONTINUE = 1;//继续播放
 	private static final int STATE_PAUSE = 2;//暂停播放
+	private boolean b = false;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		String[] array = {"播放SD卡中的音乐",
-				"播放网络URL中的音乐",
-				"播放assent中的音乐",
-				"播放raw中的音乐",
-
+		String[] array = {"播放SD卡或网络URL中的音乐(注意要申请权限)",
+				"以FileDescriptor形式播放assent或raw中的音乐。可以播放文件中指定的某一部分",
 				"暂停播放",
 				"停止播放",
 				"重新播放",};
 		setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>(Arrays.asList(array))));
-		initMediaPlayer();
 
 		//不能在onCreate（甚至onResume）中获取本ListView中的item，因为可能还没有创建呢
 		getListView().getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
@@ -52,23 +47,26 @@ public class MediaPlayerActivity extends ListActivity {
 				setPlayState(STATE_PAUSE);
 			}
 		});
+
+		initMediaPlayer();
 	}
 
 	private void initMediaPlayer() {
 		mediaPlayer = new MediaPlayer();
 		mediaPlayer.setOnCompletionListener(mp -> {
-			Toast.makeText(MediaPlayerActivity.this, "播放完毕！", Toast.LENGTH_SHORT).show();
+			Toast.makeText(MediaPlayerActivity.this, "【onCompletion】", Toast.LENGTH_SHORT).show();
 			mp.reset();//MediaPlayer同时只能播放一个音乐文件，若要播另一个音乐文件，需先设置为初始状态
 			setPlayEnable(true);
 		});
 		mediaPlayer.setOnPreparedListener(mp -> {
-			Log.i("bqt", "【准备好了】");
+			Log.i("bqt", "【onPrepared】");
 			mp.start();//只有准备好以后才能播放
 		});
 		mediaPlayer.setOnErrorListener((mp, what, extra) -> {
-			Toast.makeText(this, "【报错了】" + what + "   " + extra, Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, "【onError】" + what + "   " + extra, Toast.LENGTH_SHORT).show();
 			return false;
 		});
+		mediaPlayer.setOnSeekCompleteListener(mp -> Log.i("bqt", "【onSeekComplete】"));
 	}
 
 	@Override
@@ -85,38 +83,17 @@ public class MediaPlayerActivity extends ListActivity {
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		switch (position) {
 			case 0:
-				stop();
-				playMusicFromSDCardOrUrl(Environment.getExternalStorageDirectory() + File.separator + "voice/caravan.mp3");
+				playMusicFromSDCardOrUrl();
 				break;
 			case 1:
-				stop();
-				playMusicFromSDCardOrUrl("http://www.baiqiantao.xyz/s10_bgm.ogg");
+				playMusicFromAssentOrRawByFD();
 				break;
 			case 2:
-				stop();
-				try {
-					playMusicFromAssentOrRaw(getAssets().openFd("voice/hellow.mp3"));
-				} catch (IOException e) {
-					e.printStackTrace();
-					Toast.makeText(this, "播放失败！", Toast.LENGTH_SHORT).show();
-				}
-				break;
-			case 3:
-				stop();
-				try {
-					playMusicFromAssentOrRaw(getResources().openRawResourceFd(R.raw.hellow_tomorrow));
-				} catch (IOException e) {
-					e.printStackTrace();
-					Toast.makeText(this, "播放失败！", Toast.LENGTH_SHORT).show();
-				}
-				break;
-			case 4:
 				pause();
 				break;
-			case 5:
-				stop();
-				break;
-			case 6:
+			case 3:
+				stopPlaying();
+			case 4:
 				replay();
 				break;
 		}
@@ -125,15 +102,19 @@ public class MediaPlayerActivity extends ListActivity {
 	//******************************************************播放不同来源的音乐**********************************************
 
 	/**
-	 * 播放本地或网络音乐
+	 * 播放SD卡或网络URL中的音乐
 	 */
-	private void playMusicFromSDCardOrUrl(String path) {
-		if (path == null) return;
+	private void playMusicFromSDCardOrUrl() {
+		b = !b;
+		stopPlaying();
+		String path;
+		if (b) path = Environment.getExternalStorageDirectory() + File.separator + "voice/caravan.mp3";
+		else path = "http://www.baiqiantao.xyz/s10_bgm.ogg";
 		try {
 			mediaPlayer.setDataSource(path);//设置播放的数据源。参数可以是本地或网络路径
 			mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);//设置音频流的类型，不是必须的
-			mediaPlayer.prepareAsync();//异步准备
-			//mediaPlayer.prepare();//同步播放，prepare方法是native类型的，播放的逻辑是由c代码在新的线程里面执行的
+			mediaPlayer.prepareAsync();//For streams, you should call prepareAsync(), which returns immediately
+			//mediaPlayer.prepare();//For files, it is OK to call prepare(), which blocks until MediaPlayer is ready for playback.
 			setPlayEnable(false);//播放时将“播放”按钮设置为不可点击
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -142,17 +123,24 @@ public class MediaPlayerActivity extends ListActivity {
 	}
 
 	/**
-	 * 播放assent或Raw中的音乐
+	 * 以FileDescriptor形式播放assent或raw中的音乐。可以播放文件中指定的某一部分
 	 */
-	@TargetApi(Build.VERSION_CODES.N)
-	private void playMusicFromAssentOrRaw(AssetFileDescriptor afd) throws IOException {
-		//getAssets().openFd("caravan.mp3");
-		//getResources().openRawResourceFd(R.raw.cf_bgm1);
-
-		mediaPlayer.setDataSource(afd);
-		mediaPlayer.prepareAsync();//异步准备
-		setPlayEnable(false);//播放时将“播放”按钮设置为不可点击
+	private void playMusicFromAssentOrRawByFD() {
+		b = !b;
+		stopPlaying();
+		AssetFileDescriptor afd;
+		try {
+			if (b) afd = getAssets().openFd("voice/caravan_15s_59kb.mp3");
+			else afd = getResources().openRawResourceFd(R.raw.hellow_tomorrow);//一个10M+的超大文件
+			long offset = afd.getStartOffset(), length = afd.getDeclaredLength();
+			mediaPlayer.setDataSource(afd.getFileDescriptor(), offset + length / 2, length / 2);
+			mediaPlayer.prepareAsync();
+			setPlayEnable(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+
 	//**************************************************暂停、停止、重播***************************************************
 
 	/**
@@ -173,7 +161,7 @@ public class MediaPlayerActivity extends ListActivity {
 	/**
 	 * 停止
 	 */
-	private void stop() {
+	private void stopPlaying() {
 		if (mediaPlayer == null) return;
 
 		if (mediaPlayer.isPlaying()) mediaPlayer.stop();
@@ -189,7 +177,7 @@ public class MediaPlayerActivity extends ListActivity {
 	private void replay() {
 		if (mediaPlayer == null) return;
 
-		mediaPlayer.start();//这里调用start方法没意义，对一个已经处于Started 状态的MediaPlayer对象调用start()方法没有影响
+		mediaPlayer.start();
 		mediaPlayer.seekTo(0);//重头开始播放本音乐
 
 		setPlayState(STATE_PAUSE);
@@ -205,8 +193,6 @@ public class MediaPlayerActivity extends ListActivity {
 	private void setPlayEnable(boolean enable) {
 		getListView().getChildAt(0).setEnabled(enable);
 		getListView().getChildAt(1).setEnabled(enable);
-		getListView().getChildAt(2).setEnabled(enable);
-		getListView().getChildAt(3).setEnabled(enable);
 	}
 
 	/**
@@ -227,6 +213,6 @@ public class MediaPlayerActivity extends ListActivity {
 			mSString.setSpan(colorSpan, 0, mSString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 			mSSBuilder.append(mSString);
 		}
-		((TextView) getListView().getChildAt(4)).setText(mSSBuilder);
+		((TextView) getListView().getChildAt(2)).setText(mSSBuilder);
 	}
 }
